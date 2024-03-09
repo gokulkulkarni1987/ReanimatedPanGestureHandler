488bbace56168ac3522efcd3a5bcaee717dfee30
Gokul Kulkarni <gokul.cs5@gmail.com>
2024-03-09 23:21:20 +0530

    panning working

--- App.tsx
+++ App.tsx
@@ -6,13 +6,52 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+
+const SIZE = 100.0;
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 function App(): React.JSX.Element {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const contextX = useSharedValue(0);
+  const contextY = useSharedValue(0);
+
+  const onDrag = Gesture.Pan()
+    .onBegin(() => {
+      console.log('begin');
+      contextX.value = translateX.value;
+      contextY.value = translateY.value;
+    })
+    .onChange(event => {
+      translateX.value = event.translationX + contextX.value;
+      translateY.value = event.translationY + contextY.value;
+    })
+    .onEnd(() => {
+      console.log('on End');
+    });
+
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: translateX.value}, {translateY: translateY.value}],
+  }));
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <GestureDetector gesture={onDrag}>
+          <AnimatedView style={[styles.square, rStyle]} />
+        </GestureDetector>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -21,18 +60,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: 'rgba(0,0,256, 0.5)',
+    borderRadius: 20,
   },
 });
 
